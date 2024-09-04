98a057378e25e43ec4af2c93c45f100b7770464d
FernandoRS <soler3258@gmail.com>
2024-09-04 17:55:31 -0600

    correcion error

--- js/welcome.js
+++ js/welcome.js
@@ -14,31 +14,3 @@ btnEliminar.addEventListener("click", function(event){
     localStorage.removeItem("nombre")
     location.href="welcome.html"
 })
-
-//  rtMensaje = document-getElementById("alertMensaje");
-// Eliminar = document-getElementById ("btnEliminar");
-// dEventListener ("load", function(event){
-// JS welcome.js U X
-// 6
-// 7
-// 8
-// 9
-// 10
-// 11
-// 12
-// 13
-// 14
-// 15
-// 16
-// I ..
-// his.localStorage.getItem ("nombre")!=null)(
-// lertMensaje. innerHTML
-// = Hola,
-// «strong> ${this.localstorage-getItem"nombre")}</strong>,
-// bienvenido/a de nuevo*;
-// nombre!=null
-// Mensajestyle.display="block";
-// 1.
-// ar .addEventListener("click", function (event)(
-// • preventDefault();
-// Storage. removeItem ("nombre"); ion. href="welcome.html";
